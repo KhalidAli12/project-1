a112b9bcfe84c65b4bf91187739d2754bdbb4586
khalid <khalid.Ali.med.s@tuwaiq.edu.sa>
2021-10-13 11:18:55 +0300

    Start Extra

--- project1.playground/Contents.swift
+++ project1.playground/Contents.swift
@@ -1,124 +1,84 @@
-import UIKit
-import Darwin
-
-
-//class ToDoList {
-//    var list : [String:Int] = [:]
-//
-//
-//    func addList(key: String, value: Int) {
-//       list[key] = value
-//    }
-//
-//
-//    func UpdateList() {
-//        list.updateValue(4, forKey: "Jeddah")
-//
-//
-//    }
-//    func removeList(){
-//        list.removeValue(forKey: "Jeddah")
-//
-//}
-//
-//}
-//var test = ToDoList()
-//test.addList(key: "Bdr", value: 0)
-//test.addList(key: "Yanbu", value: 1)
-//test.addList(key: "Abha", value: 2)
-//
-//
-//test.UpdateList()
-//test.removeList()
-//print(test.list)
-
-
-
-
 struct Item {                          // create struct
     var title: String
     var status: Bool
     var id : Int
 }
-class ToDoList {                     // create class
+struct List {
+    var title: String
     var items: [Item] = []
-
-     func createItem(item: Item){  // create function
+    mutating func createItem(item: Item){  //create item for a list
         self.items.append(item)
     }
-     func readItems(){  // show or print the item
-         for item in self.items {
-             print("Task: \(item.title), Status: \(item.status), id: \(item.id)")
-         }
-       
-    }
-    func updateItem(index: Int,name:String){    // change one element
-        if index >= (items.count - 1) {print("error") }
+    mutating func updateItem(index: Int,name:String){    // change one element
+        if index > (self.items.count - 1) {print("error") }
         else{
-            list.items[index].title = name}
+            self.items[index].title = name}
         
     }
     
-    func deleteItem(index: Int){
+    mutating func deleteItem(index: Int){
         // reomve one element
-        if index >= (items.count - 1) {print("error") }
+        if index > (items.count - 1) {print("error") }
         else{
-            list.items.remove(at: index)}
+            self.items.remove(at: index)}
     }
-
-}
-var list = ToDoList()   // create
-list.createItem(item: Item(title: "Finish project", status: true, id: 1))
-list.createItem(item: Item(title: "Finish homework", status: true, id: 2))
-print(list.items.count)
-list.readItems()
-list.updateItem(index: 0,name: "ok fixed")
-list.readItems()
-
-list.deleteItem(index: 1)
-list.readItems()
-
-
-
-
-
-
-
-
-//list.updateItem(index: 0, name: "fix homework")
-// create list of item
-
-
-//print(list.items[0].title)
-//
-//list.items[0].status = true
-//
-
-//print(list.items)
-
-//list.cities.append("Rabiq") // append to list
-//print(list.cities)
-//
-//list.cities.count // read the list
-//print(list.cities)
-//
-//list.cities.remove(at: 2) // remove index 2 (Jeddah)
-//print(list.cities)
-//
-//list.cities[0] = "Abha" // change Yanbu to Abha
-//print(list.cities)
-
-//struct Araay2 {
-//    var title: [String]
-//    var iscomplet = Bool()
-//}
-//
-//var task1 = Araay2(title: ["go to saqr website at 6:00 pm" ,"code the project 01 tell 9:00 " , "go to sleep befor 11:00pm" , ])
-
-//for i in  {
-//    print(task1.iscomplet)
     
-//}
-//var T = Araay2(title: ["Khalid" , "Ahmed" , "Ali"], iscomplet: true)
-//print(T.iscomplet)
+    mutating func readItems(){  // show or print the item
+        for item in self.items{
+            print("Task: \(item.title), Status: \(item.status), id: \(item.id)")
+            
+        }
+        
+    }
+}
+class ToDoList {                     // create class
+    var lists: [List] = []
+    
+}
 
+var myLists = ToDoList()
+myLists.lists.append(List(title: "Work" , items: [Item(title: "Fix project", status: true, id: 0)]))
+myLists.lists.append(List(title: "Personal", items: [Item(title: "Buy water", status: false, id: 1)]))
+
+myLists.lists[0].readItems()
+myLists.lists[1].readItems()
+//myLists.lists.remove(at: 0)
+//myLists.lists
+//myLists.lists[1].readItems()
+
+myLists.lists[0].updateItem(index: 0, name: "project completed")
+myLists.lists[0].readItems()
+myLists.lists[0].deleteItem(index: 0)
+myLists.lists[0].readItems()
+myLists.lists[1].readItems()
+//myLists.lists[1].updateItem(index: 0, name: "by pepsi")
+
+//myLists.lists[1].readItems()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//var list = ToDoList()   // create
+//list.createItem(item: Item(title: "Finish project", status: true, id: 1))
+//list.createItem(item: Item(title: "Finish homework", status: true, id: 2))
+
+//list.readItems()
+//list.updateItem(index: 0,name: "ok fixed")
+//list.readItems()
+//
+//list.deleteItem(index: 1)
+//list.readItems()
